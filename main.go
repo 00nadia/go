12366902d1b148086842f93f7a508c47a68fd7d1
00nadia <nadia_tyb@poczta.fm>
2024-08-08 12:40:59 +0200

    update

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -15,10 +17,17 @@ const (
 	dbname   = "dbname"
 )
 
+type info struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	Pet       string `json:"pet"`
+}
+
 func main() {
 
-	psglInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psglInfo)
+	SQLInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", SQLInfo)
 	if err != nil {
 		panic(err)
 	}
@@ -29,35 +38,38 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Successfully connected!")
-	//update
-	sqlStatement := `
-	UPDATE test 
-	SET age = $1, pet =$2, last_name = $3
-	WHERE first_name = $4;`
 
-	_, err = db.Exec(sqlStatement, 35, "bat", "Wayne", "Bruce")
-	if err != nil {
-		panic(err)
-	}
-	//print
 	rows, err := db.Query("SELECT * FROM test")
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
+	var infoTest []info = make([]info, 0)
 	for rows.Next() {
-		var firstName string
-		var lastName string
-		var age int
-		var pet string
-		err = rows.Scan(&firstName, &lastName, &age, &pet)
+		content := info{}
+		err = rows.Scan(&content.FirstName, &content.LastName, &content.Age, &content.Pet)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(firstName, lastName, age, pet)
+		infoTest = append(infoTest, content)
 	}
+	fmt.Println(infoTest)
 	err = rows.Err()
 	if err != nil {
 		panic(err)
 	}
+
+	router := gin.Default()
+
+	router.GET("/show", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, infoTest)
+	})
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello")
+
+	})
+
+	router.Run("localhost:4000")
+
 }
